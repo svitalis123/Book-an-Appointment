0159ed26e54a3ab203dd3d54bc7b9610ba45c52c
shuhad786 <shuhad.loofer@gmail.com>
2023-03-09 11:13:04 +0200

    Add admin to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_101532) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_090549) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_101532) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 
 end
