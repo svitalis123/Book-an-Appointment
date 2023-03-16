66a63a1b3a6a6ea7a4effa2e776dd337bb58bf3f
shuhad786 <shuhad.loofer@gmail.com>
2023-03-16 16:38:04 +0200

    Correct linter errors

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class ServicesController < ApplicationController
   before_action :set_service, only: %i[show update destroy]
 
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Service < ApplicationRecord
   validates :service, presence: true
   validates :photo, presence: true
