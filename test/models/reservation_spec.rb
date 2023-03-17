73357cf46ae120c3bf96fb92424c4e94fe2a3e41
svitalis123 <vitalis.wilson@yahoo.com>
2023-03-17 13:03:08 +0300

    reservation test

--- test/models/reservation_spec.rb
+++ test/models/reservation_spec.rb
@@ -27,24 +27,24 @@ RSpec.describe Reservation, type: :model do
 
 
         it "resrevation validate" do
-            subject.model = model
-            expect(subject).to_not be_valid
+            subject.model = "model"
+            expect(subject).to eql("model")
         end
         it "resrevation validate" do
-            subject.year = year
-            expect(subject).to_not be_valid
+            subject.year = 2010
+            expect(subject).to eql(2010)
         end
         it "resrevation validate" do
-            subject.vehicle = vehicle
-            expect(subject).to_not be_valid
+            subject.vehicle = "vehicle"
+            expect(subject).to eql("vehicle")
         end
         it "resrevation validate" do
-            subject.color = color
-            expect(subject).to_not be_valid
+            subject.color = "color"
+            expect(subject).to eql("color")
         end
         it "resrevation validate" do
-            subject.service = service
-            expect(subject).to_not be_valid
+            subject.service = "service"
+            expect(subject).to eql("service")
         end
     end
 end
