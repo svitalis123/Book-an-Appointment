e160d355f406eeb088ee68f40371895e5ed7af63
svitalis123 <vitalis.wilson@yahoo.com>
2023-03-17 12:47:42 +0300

    service rest test

--- test/models/service_spec.rb
+++ test/models/service_spec.rb
@@ -27,5 +27,63 @@ RSpec.describe Service, type: :model do
         end
     end
 
-    
+    context " description should be validated" do
+        it "validate description" do 
+            subject.service = "Test description"
+            expect(subject).to eql("Test description")
+        end
+
+        it "validate description" do 
+            subject.service = nil
+            expect(subject).to_not be_valid
+        end
+    end
+
+    context " labour should be validated" do
+        it "validate labour" do 
+            subject.service = "Test labour"
+            expect(subject).to eql("Test labour")
+        end
+
+        it "validate labour" do 
+            subject.service = nil
+            expect(subject).to_not be_valid
+        end
+    end
+
+    context " engineer should be validated" do
+        it "validate engineer" do 
+            subject.service = "Test engineer"
+            expect(subject).to eql("Test engineer")
+        end
+
+        it "validate engineer" do 
+            subject.service = nil
+            expect(subject).to_not be_valid
+        end
+    end
+
+    context " price should be validated" do
+        it "validate price" do 
+            subject.service = 100
+            expect(subject).to eql(100)
+        end
+
+        it "validate price" do 
+            subject.service = nil
+            expect(subject).to_not be_valid
+        end
+    end
+
+    context " duration should be validated" do
+        it "validate duration" do 
+            subject.service = 60
+            expect(subject).to eql(60)
+        end
+
+        it "validate duration" do 
+            subject.service = nil
+            expect(subject).to_not be_valid
+        end
+    end
 end
